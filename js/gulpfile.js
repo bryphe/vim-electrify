74f246983907dcea8c073ecd41a19f0870618012
extr0py <extr0py@extropygames.com>
2016-02-14 17:15:03 -0800

    Start adding client/server architecture

--- js/gulpfile.js
+++ js/gulpfile.js
@@ -3,14 +3,23 @@ var gulp = require("gulp");
 var ts = require("gulp-typescript");
 
 var serverTsConfigPath = path.join(__dirname, "src/server/tsconfig.json");
+var clientTsConfigPath = path.join(__dirname, "src/client/tsconfig.json");
 
-var tsProject = ts.createProject(serverTsConfigPath);
+var tsServerProject = ts.createProject(serverTsConfigPath);
+var tsClientProject = ts.createProject(clientTsConfigPath);
 
 gulp.task("build:server", function () {
-    var tsResult = tsProject.src()
-        .pipe(ts(tsProject));
+    var tsResult = tsServerProject.src()
+        .pipe(ts(tsServerProject));
 
     return tsResult.js.pipe(gulp.dest(path.join(__dirname, "lib/server")));
 });
 
-gulp.task("default", ["build:server"]);
+gulp.task("build:client", function () {
+    var tsResult = tsClientProject.src()
+        .pipe(ts(tsClientProject));
+
+    return tsResult.js.pipe(gulp.dest(path.join(__dirname, "lib/client")));
+});
+
+gulp.task("default", ["build:server", "build:client"]);
