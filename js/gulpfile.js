f0edb7d32700225ff3abe2bd95c2e2b21b7aa5c7
extr0py <extropy@extropygames.com>
2016-06-17 20:59:33 -0700

    Add copying html to gulpfile

--- js/gulpfile.js
+++ js/gulpfile.js
@@ -29,6 +29,10 @@ tsProjects.forEach(function (project) {
     });
 });
 
+gulp.task("copy:html", () => {
+    return gulp.src(path.join(__dirname, "src", "server", "**", "*.html"))
+        .pipe(gulp.dest(path.join(__dirname, "lib", "server")));
+});
 gulp.task("copy:assets", () => {
     return gulp.src(path.join(__dirname, "src", "assets", "**", "*.*"))
         .pipe(gulp.dest(path.join(__dirname, "lib", "assets")));
@@ -39,6 +43,7 @@ var buildTasks = tsProjects.map(function (project) {
 });
 
 buildTasks.push("copy:assets");
+buildTasks.push("copy:html");
 
 var typingsTasks = tsProjects.map(function (project) {
     return "install-typings:" + project
