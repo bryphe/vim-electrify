9c2145d55f39db8d2ff0bd904ed5b4bc25dd2d48
extr0py <extr0py@extropygames.com>
2016-07-16 10:24:54 -0700

    Use BrowserWindowPluginHostFactory

---
+++ js/src/server/BrowserWindowPluginHostFactory.ts
@@ -0,0 +1,20 @@
+import { IPluginHost } from "./IPluginHost";
+import { IPluginHostFactory } from "./IPluginHostFactory";
+import BrowserWindowPluginHost from "./BrowserWindowPluginHost";
+
+export default class BrowserWindowPluginHostFactory implements IPluginHostFactory {
+
+    private _io: any;
+    private _port: any;
+    private _channel: number = 0;
+
+    constructor(io: any, port: number) {
+        this._io = io;
+        this._port = port;
+    }
+
+    public createPluginHost(): IPluginHost {
+        this._channel++;
+        return new BrowserWindowPluginHost(this._io, this._port, this._channel);
+    }
+}
